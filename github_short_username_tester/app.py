997fc10892b7930e97af212b0ac459c3a8ffd665
pas <mihnevich.pavel.d@gmail.com>
2021-08-21 08:52:02 +0300

    open file for each write separately

--- github_short_username_tester/app.py
+++ github_short_username_tester/app.py
@@ -26,12 +26,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from os import environ
 from sys import argv
 from time import sleep
-from winsound import Beep
 
 
 symbs: list = [chr(x) for x in range(97, 123)] + [chr(x) for x in range(48, 58)]
 n_symbs: list = [chr(x) for x in range(97, 123)] + [chr(x) for x in range(48, 58)] + ['-']
-result_file = open('available.txt', 'a')
 
 s = int(argv[1]) if len(argv) > 1 else 3
 e = int(argv[2]) if len(argv) > 2 else 0
@@ -76,8 +74,9 @@ def check_nickname(nn: str):
                 while True:
                     sleep(1)
                     if result_message.text == nn + f + " is available.":
+                        with open('available.txt', 'a') as file:
+                            file.write(nn + f + '\n')
                         print(result_message.text)
-                        result_file.write(nn + f + '\n')
                         break
                     elif (
                         result_message.text[: 28 + len(nn)]
