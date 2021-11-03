c8233225e98cb6eac97d09eca05bbdafbf361f17
pas <mihnevich.pavel.d@gmail.com>
2021-11-03 19:23:21 +0300

    re-check 3symb available

--- github_short_usernames/app.py
+++ github_short_usernames/app.py
@@ -7,10 +7,12 @@ Full cycle with (3,0) args and 1s sleep takes at least 14 hours.
                 (4,4) args and 1s sleep takes at least 20.5 days ¯|_(ツ)_/¯
 You can change "symbs" list to separate full cycle into parts.
 Input: [
+        github_login: str
+        github_password: str （＾～＾）
         max_length: int (default&recomended – 3);
         min_length: int (default – 0). It makes sense if you've
                     already completed (3,0) and want to start (4,4);
-        start_nn: str (default 'a'). Skip all values that are less than this attribute.
+        start_nn: str (default '0'). Skip all values that are less than this attribute.
     ]
 Output: [
         realtime print available nicknames, errors (long responce);
@@ -18,7 +20,6 @@ Output: [
     ]
 '''
 
-from os import environ
 from sys import argv
 from time import sleep
 
@@ -32,16 +33,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 symbs: list = [chr(x) for x in range(97, 123)] + [chr(x) for x in range(48, 58)]
 n_symbs: list = symbs + ['-']
 
-max_length = int(argv[1]) if len(argv) > 1 else 3
-min_length = int(argv[2]) if len(argv) > 2 else 0
-first_value = str(argv[3]) if len(argv) > 3 else '0'
+max_length = int(argv[3]) if len(argv) > 3 else 3
+min_length = int(argv[4]) if len(argv) > 4 else 0
+first_value = str(argv[5]) if len(argv) > 5 else '0'
 
 driver = webdriver.Firefox()
 driver.get("https://github.com/settings/admin")
 login_input = driver.find_element_by_id("login_field")
 password_input = driver.find_element_by_id("password")
-login_input.send_keys(environ['github_login'])
-password_input.send_keys(environ['github_password'])
+login_input.send_keys(argv[1])
+password_input.send_keys(argv[2])
 password_input.send_keys(Keys.RETURN)
 
 sleep(3)
